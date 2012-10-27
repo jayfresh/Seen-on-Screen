eaada7656500ddab70560a24b0631a500cff91e6
Jonathan Lister <jonathan@withjandj.com>
2012-10-27 15:12:45 +0100

    changing structure of team carousel to fit lots of images

--- themes/sos/archive-sos_team.php
+++ themes/sos/archive-sos_team.php
@@ -4,20 +4,42 @@ get_header();
 ?>
 
 			<div class="teamCarousel">
-				<div class="teamCarouselStrip">
-					<div class="teammember"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/team/anthony_profile.jpg"></div>
-					<div class="teammember"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/team/nicole_profile.jpg"></div>
-					<div class="teammember"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/team/pam_profile.jpg"></div>
+				<div class="teamCarouselStripContainer">
+					<div class="teamCarouselStrip">
+						<?php if ( have_posts() ) :
+							while ( have_posts() ) : the_post(); ?>
+						<div class="teammember">
+							<?php if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+							  the_post_thumbnail();
+							} else { ?>
+							<img src="<?php bloginfo('stylesheet_directory'); ?>/images/team/shaun_profile.jpg" title="no profile">
+							<?php } ?>
+							<div class="infobox">
+								<h2 class="teammember"><?php the_title(); ?></h2>
+								<ul class="credits">
+								<?php
+								$creditsList = get_post_meta($post->ID,'_credits',true);
+								$creditsList = explode(',', $creditsList);
+								foreach($creditsList as $credit) : ?>
+									<li><?php echo trim($credit); ?></li>
+								<?php endforeach; ?>
+								</ul>
+								<?php the_content(); ?>
+							</div>
+						</div>
+							<?php endwhile;
+						endif; ?>
+					</div>
 				</div>
 				<div class="infobox">
-					<h2 class="teammember">Team member</h2>
+					<!--<h2 class="teammember">Team member</h2>
 					<ul class="credits">
 						<li>Rihanna</li>
 						<li>Beyonc&eacute;</li>
 						<li>Janet Jackson</li>
 						<li>Jessie J</li>
 					</ul>
-					<p>Bio goes here bio goes here bio goes here bio goes here bio goes here bio goes here</p>
+					<p>Bio goes here bio goes here bio goes here bio goes here bio goes here bio goes here</p>-->
 				</div>
 			</div>
 
