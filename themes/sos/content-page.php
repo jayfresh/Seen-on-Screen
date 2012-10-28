52ef9998334ec37ff6f3d005becce997c530b3cf
Jonathan Lister <jonathan@withjandj.com>
2012-10-28 14:21:08 +0000

    more pre-JS-load fixes

--- themes/sos/content-page.php
+++ themes/sos/content-page.php
@@ -5,10 +5,14 @@
 					<h1><?php the_title(); ?></h1>
 					<?php the_content(); ?>
 				</div>
-				<div class="eventbox left carouselContainer">
-					<?php if ( has_post_thumbnail() ) {
-						attachment_toolbox($size = 'content-page', 'carousel');
-					} else { ?>
-					<img title="no featured image" src="<?php bloginfo('stylesheet_directory'); ?>/images/roughs/rihanna-event.png">
+				<div class="eventbox left">
+					<?php if ( has_post_thumbnail() ) { ?>
+					<div class="carouselContainer eventImageContainer">
+						<?php attachment_toolbox($size = 'content-page', 'carousel'); ?>
+					</div>
+					<?php } else { ?>
+					<div class="eventImageContainer">
+						<img title="no featured image" src="<?php bloginfo('stylesheet_directory'); ?>/images/roughs/rihanna-event.png">
+					</div>
 					<?php } ?>
 				</div>
