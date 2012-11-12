0c1f8f4d25b729cf062da29408eb7b970dd20019
Jonathan Lister <jonathan@withjandj.com>
2012-11-12 21:09:37 +0000

    sorting press images left float

--- themes/sos/page-press.php
+++ themes/sos/page-press.php
@@ -3,7 +3,7 @@
 get_header();
 ?>
 
-			<div class="contentPage">
+			<div class="contentPage" id="pressPage">
 				<?php if ( have_posts() ) :
 					while ( have_posts() ) : the_post(); ?>
 				<div class="contentColumn left">
