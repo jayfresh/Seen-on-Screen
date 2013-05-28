57584dde8a8a37b1a7ed7948c8c53c1b69733a33
Jonathan Lister <jonathan@withjandj.com>
2013-05-28 23:24:23 +0200

    Adding some hilarious Justin Timberlake action

--- themes/sos/page-90s.php
+++ themes/sos/page-90s.php
@@ -50,6 +50,9 @@ get_header();
 							<li>
 								<iframe width="610" height="343" src="http://www.youtube.com/embed/vMftKbsKHWg?rel=0" frameborder="0" allowfullscreen></iframe>
 							</li>
+							<li>
+								<iframe width="610" height="343" src="http://www.youtube.com/embed/is6gtilerPk?rel=0" frameborder="0" allowfullscreen></iframe>
+							</li>
 							<?php foreach($images as $image) {
 								$attsrc  = wp_get_attachment_image_src($image->ID, $size);
 								$atttitle = apply_filters('the_title',$image->post_title); ?>
