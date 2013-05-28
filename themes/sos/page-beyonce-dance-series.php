102831907a7c6efa5904c15f8bb3aa902c0d0360
Jonathan Lister <jonathan@withjandj.com>
2013-05-28 23:19:46 +0200

    Adding and removing vids

--- themes/sos/page-beyonce-dance-series.php
+++ themes/sos/page-beyonce-dance-series.php
@@ -27,9 +27,6 @@ get_header();
 								<iframe width="610" height="343" src="http://www.youtube.com/embed/gFDdHJUY53M?rel=0" frameborder="0" allowfullscreen></iframe>
 							</li>
 							<li>
-								<iframe width="610" height="343" src="http://www.youtube.com/embed/ArDXxTsJJoo?rel=0" frameborder="0" allowfullscreen></iframe>
-							</li>
-							<li>
 								<iframe width="610" height="343" src="http://www.youtube.com/embed/4m1EFMoRFvY?rel=0" frameborder="0" allowfullscreen></iframe>
 							</li>
 							<li>
@@ -42,10 +39,10 @@ get_header();
 								<iframe width="610" height="343" src="http://www.youtube.com/embed/rNM5HW13_O8?rel=0" frameborder="0" allowfullscreen></iframe>
 							</li>
 							<li>
-								<iframe width="610" height="343" src="http://www.youtube.com/embed/hbnPkK76Ask?rel=0" frameborder="0" allowfullscreen></iframe>
+								<iframe width="610" height="343" src="http://www.youtube.com/embed/oAmnkPUFMHg?rel=0" frameborder="0" allowfullscreen></iframe>
 							</li>
 							<li>
-								<iframe width="610" height="343" src="http://www.youtube.com/embed/2XY3AvVgDns?rel=0" frameborder="0" allowfullscreen></iframe>
+								<iframe width="610" height="343" src="http://www.youtube.com/embed/VyL73k5AMbQ?rel=0" frameborder="0" allowfullscreen></iframe>
 							</li>
 							<?php foreach($images as $image) {
 								$attsrc  = wp_get_attachment_image_src($image->ID, $size);
