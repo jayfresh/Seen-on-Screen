8f1ffc474a9b33aab1b9404b9833749a8ee2bfa8
Jonathan Lister <jonathan@withjandj.com>
2013-06-21 14:13:15 +0100

    changing the way default page template shows images: if there is a featured image, it just shows that; if not, it shows all attachments (note, they have to be attachments!); if no attachments, shows the no-image (Rihanna) image

--- themes/sos/content-page.php
+++ themes/sos/content-page.php
@@ -7,12 +7,14 @@
 				</div>
 				<div class="eventbox plainbox left">
 					<?php if ( has_post_thumbnail() ) { ?>
-					<div class="carouselContainer eventImageContainer">
-						<?php attachment_toolbox('content-page', 'carousel'); ?>
+					<div class="eventImageContainer">
+						<?php the_post_thumbnail('content-page'); ?>
 					</div>
 					<?php } else { ?>
-					<div class="eventImageContainer">
-						<img title="no featured image" src="<?php bloginfo('stylesheet_directory'); ?>/images/roughs/rihanna-event.png">
+					<div class="carouselContainer eventImageContainer">
+						<?php if(!attachment_toolbox('content-page', 'carousel')) { ?>
+							<img title="no featured image" src="<?php bloginfo('stylesheet_directory'); ?>/images/roughs/rihanna-event.png">
+						<?php } ?>
 					</div>
 					<?php } ?>
 				</div>
