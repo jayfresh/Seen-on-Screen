24b6685838153fa6bcfe1bbd695d2150cdfdf7ff
Jonathan Lister <jonathan@withjandj.com>
2013-06-21 13:55:03 +0100

    making videos show up even if there is no attachment

--- themes/sos/page-beyonce.php
+++ themes/sos/page-beyonce.php
@@ -11,17 +11,6 @@ get_header();
 				</div>
 				<div class="eventbox plainbox left">
 					<div class="carouselContainer eventImageContainer">
-					<?php
-					$size = 'content-page';
-					if($images = get_children(array(
-						'post_parent'    => get_the_ID(),
-						'post_type'      => 'attachment',
-						'numberposts'    => -1, // show all
-						'post_status'    => null,
-						'post_mime_type' => 'image',
-						'order'          => 'ASC',
-						'orderby'        => 'menu_order',
-					))) { ?>
 						<ul class="carousel">
 							<li>
 								<iframe width="610" height="343" src="http://www.youtube.com/embed/gFDdHJUY53M?rel=0" frameborder="0" allowfullscreen></iframe>
@@ -44,13 +33,24 @@ get_header();
 							<li>
 								<iframe width="610" height="343" src="http://www.youtube.com/embed/VyL73k5AMbQ?rel=0" frameborder="0" allowfullscreen></iframe>
 							</li>
-							<?php foreach($images as $image) {
+							<?php
+							$size = 'content-page';
+							if($images = get_children(array(
+								'post_parent'    => get_the_ID(),
+								'post_type'      => 'attachment',
+								'numberposts'    => -1, // show all
+								'post_status'    => null,
+								'post_mime_type' => 'image',
+								'order'          => 'ASC',
+								'orderby'        => 'menu_order',
+							))) {
+							foreach($images as $image) {
 								$attsrc  = wp_get_attachment_image_src($image->ID, $size);
 								$atttitle = apply_filters('the_title',$image->post_title); ?>
 							<li><img alt="<?php echo $atttitle; ?>" src="<?php echo $attsrc[0]; ?>"></li>
-							<?php } ?>
+							<?php }
+							} ?>
 						</ul>
-					<?php } ?>
 					</div>
 				</div>
 				<?php endwhile;
