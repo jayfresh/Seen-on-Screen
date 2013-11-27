55c8e5f13a2f6cf5719935fc1e64f888c41d1d56
jayfresh <jonathan@penrosestudio.com>
2013-11-27 13:22:00 +0000

    adding pages for Christmas special and Dance Movies

---
+++ themes/sos/page-sos-christmas-special.php
@@ -0,0 +1,43 @@
+<?php
+/* for SoS Christmas Special workshop page */
+get_header();
+?>
+    	<div class="contentPage">
+				<?php if ( have_posts() ) :
+					while ( have_posts() ) : the_post(); ?>
+				<div class="contentColumn left">
+					<h1><?php the_title(); ?></h1>
+					<?php the_content(); ?>
+				</div>
+				<div class="eventbox plainbox left">
+					<div class="carouselContainer eventImageContainer">
+					<?php
+					$size = 'content-page';
+					if($images = get_children(array(
+						'post_parent'    => get_the_ID(),
+						'post_type'      => 'attachment',
+						'numberposts'    => -1, // show all
+						'post_status'    => null,
+						'post_mime_type' => 'image',
+						'order'          => 'ASC',
+						'orderby'        => 'menu_order',
+					))) { ?>
+						<ul class="carousel">
+							<li>
+								<iframe width="610" height="343" src="http://www.youtube.com/embed/yXQViqx6GMY?rel=0" frameborder="0" allowfullscreen></iframe>
+							</li>
+							<?php foreach($images as $image) {
+								$attsrc  = wp_get_attachment_image_src($image->ID, $size);
+								$atttitle = apply_filters('the_title',$image->post_title); ?>
+							<li><img alt="<?php echo $atttitle; ?>" src="<?php echo $attsrc[0]; ?>"></li>
+							<?php } ?>
+						</ul>
+					<?php } ?>
+					</div>
+				</div>
+				<?php endwhile;
+				endif; ?>
+			</div>
+<?php
+get_footer();
+?>
