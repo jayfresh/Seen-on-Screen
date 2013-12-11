834a63df50971fc04dcbbfcf9b0fcd5e9990878a
jayfresh <jonathan@penrosestudio.com>
2013-12-11 22:16:29 +0000

    Adding the Twerktorial

--- themes/sos/page-twerkshop.php
+++ themes/sos/page-twerkshop.php
@@ -24,6 +24,9 @@ get_header();
 					))) { ?>
 						<ul class="carousel">
 							<li>
+								<iframe width="610" height="343" src="http://www.youtube.com/embed/TG9XqNdSiEY?rel=0" frameborder="0" allowfullscreen></iframe>
+							</li>
+							<li>
 								<iframe width="610" height="343" src="http://www.youtube.com/embed/1-TTNdyIMAE?rel=0" frameborder="0" allowfullscreen></iframe>
 							</li>
 							<li>
