244ea226afbcd9579ecba60448a1f7871ae9dab0
jayfresh <jonathan@penrosestudio.com>
2014-01-02 13:06:04 +0000

    adding more style

--- themes/sos/page-membership-two.php
+++ themes/sos/page-membership-two.php
@@ -1,28 +1,32 @@
 <?php
 /* for Membership page */
 get_header(); ?>
-	<div class="contentPage">	
+	<div class="contentPage" id="membership">	
 	<?php if ( have_posts() ) :
 		while ( have_posts() ) : the_post(); ?>
-		<h1><?php the_post(); ?></h1>
+		<h1><?php the_title(); ?></h1>
 			<div class="margintop">
 				<div class="grid6col left">
 				<?php the_content(); ?>
 				</div>
 				<div class="grid6col left">
 					<div class="grid6col">
-					  <img src="<?php bloginfo('stylesheet_directory'); ?>/images/aaa-membership.png" alt="Access All Areas membership">
-					  <p>Unlimited Access to all SOS Classes and Workshops</p>
-					  <p><span>&pound;75</span> per month</p>
-					  <form>
-  					  paypal button here
-					  </form>
-					  <img src="<?php bloginfo('stylesheet_directory'); ?>/images/vip-membership.png" alt="VIP membership">
-					  <p>Access to all Weekend Workshops</p>
-					  <p><span>&pound;50</span> per month</p>
-					  <form>
-  					  paypal button here
-					  </form>					  
+					  <div class="badge">
+  					  <img src="<?php bloginfo('stylesheet_directory'); ?>/images/aaa-membership.png" alt="Access All Areas membership">
+  					  <p>Unlimited Access to all SOS Classes and Workshops</p>
+  					  <p class="aaa"><span class="large">&pound;75</span> per month</p>
+  					  <form>
+    					  paypal button here
+  					  </form>
+					  </div>
+					  <div class="badge">
+  					  <img src="<?php bloginfo('stylesheet_directory'); ?>/images/vip-membership.png" alt="VIP membership">
+  					  <p>Access to all Weekend Workshops</p>
+  					  <p><span class="large">&pound;50</span> per month</p>
+  					  <form>
+    					  paypal button here
+  					  </form>					  
+					  </div>
 					</div>
 				</div>
 			</div>			
