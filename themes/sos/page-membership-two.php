9caba775ee57909adfbdfeec276f595378f36ca0
jayfresh <jonathan@penrosestudio.com>
2014-01-02 13:16:26 +0000

    more styles

--- themes/sos/page-membership-two.php
+++ themes/sos/page-membership-two.php
@@ -1,7 +1,7 @@
 <?php
 /* for Membership page */
 get_header(); ?>
-	<div class="contentPage" id="membership">	
+	<div class="contentPage blackContainer" id="membership">	
 	<?php if ( have_posts() ) :
 		while ( have_posts() ) : the_post(); ?>
 		<h1><?php the_title(); ?></h1>
@@ -9,25 +9,23 @@ get_header(); ?>
 				<div class="grid6col left">
 				<?php the_content(); ?>
 				</div>
-				<div class="grid6col left">
-					<div class="grid6col">
-					  <div class="badge">
-  					  <img src="<?php bloginfo('stylesheet_directory'); ?>/images/aaa-membership.png" alt="Access All Areas membership">
-  					  <p>Unlimited Access to all SOS Classes and Workshops</p>
-  					  <p class="aaa"><span class="large">&pound;75</span> per month</p>
-  					  <form>
-    					  paypal button here
-  					  </form>
-					  </div>
-					  <div class="badge">
-  					  <img src="<?php bloginfo('stylesheet_directory'); ?>/images/vip-membership.png" alt="VIP membership">
-  					  <p>Access to all Weekend Workshops</p>
-  					  <p><span class="large">&pound;50</span> per month</p>
-  					  <form>
-    					  paypal button here
-  					  </form>					  
-					  </div>
-					</div>
+				<div class="grid5col marginleft left">
+				  <div class="badge">
+					  <img src="<?php bloginfo('stylesheet_directory'); ?>/images/aaa-membership.png" alt="Access All Areas membership">
+					  <p>Unlimited Access to all SOS Classes and Workshops</p>
+					  <p class="aaa"><span class="large">&pound;75</span> per month</p>
+					  <form>
+  					  paypal button here
+					  </form>
+				  </div>
+				  <div class="badge">
+					  <img src="<?php bloginfo('stylesheet_directory'); ?>/images/vip-membership.png" alt="VIP membership">
+					  <p>Access to all Weekend Workshops</p>
+					  <p><span class="large">&pound;50</span> per month</p>
+					  <form>
+  					  paypal button here
+					  </form>					  
+				  </div>
 				</div>
 			</div>			
 			<br class="clearboth">
