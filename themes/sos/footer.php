55e15d05e0459c626b4f7ef64d69810b5d778d9f
jayfresh <jonathan@penrosestudio.com>
2014-08-28 11:14:51 +0100

    fix typo

--- themes/sos/footer.php
+++ themes/sos/footer.php
@@ -1,7 +1,7 @@
 			<div class="push"></div>
 		</div>
 		<div class="footer">
-            <p class="right"><a href="/privacy-policy" title="Privacy Policy">Privacy Policy</a><p>
+            <p class="right"><a href="/privacy-policy" title="Privacy Policy">Privacy Policy</a></p>
             <p>&copy; 2011-2014 Seen On Screen Fitness &amp; Events Ltd</p>
         </div>
 		<?php wp_footer(); ?>
