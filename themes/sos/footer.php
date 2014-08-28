2015c461a8ec794215cf0767eb85a27b1870a772
jayfresh <jonathan@penrosestudio.com>
2014-08-28 11:12:57 +0100

    Added footer link to privacy policy and updated copyright

--- themes/sos/footer.php
+++ themes/sos/footer.php
@@ -1,7 +1,8 @@
 			<div class="push"></div>
 		</div>
 		<div class="footer">
-            <p>&copy; 2011-2012 Seen On Screen Fitness &amp; Events Ltd</p>
+            <p class="right"><a href="/privacy-policy" title="Privacy Policy">Privacy Policy</a><p>
+            <p>&copy; 2011-2014 Seen On Screen Fitness &amp; Events Ltd</p>
         </div>
 		<?php wp_footer(); ?>
 	</body>
