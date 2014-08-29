364a149326045b57b0411a85ff87a0bc2061c29a
jayfresh <jonathan@penrosestudio.com>
2014-08-29 15:34:54 +0100

    Fixing typo

--- themes/sos/footer.php
+++ themes/sos/footer.php
@@ -2,7 +2,7 @@
 		</div>
 		<div class="footer">
             <p class="right"><a href="/privacy-policy" title="Privacy Policy">Privacy Policy</a></p>
-            <p class="right"><a href=" http://www.netbanx.com"><img src=" http://support.optimalpayments.com/images/powered_by_netbanx_visa_mc.jpg" border="0" width="136" height="86" alt="NETBANX By Optimal Payments"></a>&nbsp;</p>
+            <p class="right"><a href="http://www.netbanx.com"><img src=" http://support.optimalpayments.com/images/powered_by_netbanx_visa_mc.jpg" border="0" width="136" height="86" alt="NETBANX By Optimal Payments"></a>&nbsp;</p>
             <p>&copy; 2011-2014 Seen On Screen Fitness &amp; Events Ltd</p>
         </div>
 		<?php wp_footer(); ?>
