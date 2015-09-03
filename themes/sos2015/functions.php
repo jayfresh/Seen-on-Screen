f182e5c99d67ca2d8cd0bf1ee351c8ad563b459c
jayfresh <jonathan@penrosestudio.com>
2015-09-03 23:40:21 +0100

    Press page now pulls from press quotes; quotes pages now have a URL field for setting the link URL

--- themes/sos2015/functions.php
+++ themes/sos2015/functions.php
@@ -55,7 +55,8 @@ function sos_register_custom_post_types() {
             'show_ui' => true, // so we can edit these from wp-admin
             'exclude_from_search' => false, // to allow WP_Query to find these
             'has_archive' => false,
-            'supports' => array('title','editor','thumbnail')
+            'supports' => array('title','editor','thumbnail'),
+            'register_meta_box_cb' => 'sos_add_quote_metabox'
         )
     );
 
@@ -124,6 +125,58 @@ function sos_studio_save_meta($post_id, $post) {
 }
 
 add_action('save_post', 'sos_studio_save_meta', 1, 2);
+
+/* add metaboxes to quote post type */
+
+function sos_add_quote_metabox() {
+	add_meta_box('quote_metabox', 'Quote URL', 'quote_metabox', 'sos_quote', 'normal', 'default');
+}
+
+function quote_metabox() {
+    global $post;
+
+    // Noncename needed to verify where the data originated
+    echo '<input type="hidden" name="quotemeta_noncename" id="quotemeta_noncename" value="' .
+    wp_create_nonce( plugin_basename(__FILE__) ) . '" />';
+
+    $url = get_post_meta($post->ID, '_url', true);
+
+    echo '<label for="_address">URL</label><input type="text" id="_url" name="_url" value="' . $url  . '" class="widefat" />';
+}
+
+function sos_quote_save_meta($post_id, $post) {
+
+    // verify this came from the our screen and with proper authorization,
+    // because save_post can be triggered at other times
+    if ( !wp_verify_nonce( $_POST['quotemeta_noncename'], plugin_basename(__FILE__) )) {
+    return $post->ID;
+    }
+
+    // Is the user allowed to edit the post or page?
+    if ( !current_user_can( 'edit_post', $post->ID ))
+        return $post->ID;
+
+    // OK, we're authenticated: we need to find and save the data
+    // We'll put it into an array to make it easier to loop though.
+
+    $quote_meta['_url'] = $_POST['_url'];
+
+    // Add values of $events_meta as custom fields
+
+    foreach ($quote_meta as $key => $value) { // Cycle through the $events_meta array!
+        if( $post->post_type == 'revision' ) return; // Don't store custom data twice
+        $value = implode(',', (array)$value); // If $value is an array, make it a CSV (unlikely)
+        if(get_post_meta($post->ID, $key, FALSE)) { // If the custom field already has a value
+            update_post_meta($post->ID, $key, $value);
+        } else { // If the custom field doesn't have a value
+            add_post_meta($post->ID, $key, $value);
+        }
+        if(!$value) delete_post_meta($post->ID, $key); // Delete if blank
+    }
+
+}
+
+add_action('save_post', 'sos_quote_save_meta', 1, 2);
 
 /* add meta boxes to team post type */
 
--- themes/sos2015/page-press.php
+++ themes/sos2015/page-press.php
@@ -7,34 +7,24 @@ get_header();
 	<div class="container-holder">
 		<h2>As Seen In</h2>
 		<ul class="logo-list">
-			<li><a href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-grazia.png" alt="Grazia" width="216" height="68"></a></li>
-			<li><a href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-buzzfeed.png" alt="BuzzFeed" width="338" height="60"></a></li>
-			<li><a href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-cosmopolitan.png" alt="Cosmopolitan" width="324" height="64"></a></li>
-			<li><a href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-les.png" alt="London Evening Standard" width="282" height="116"></a></li>
-			<li><a href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>/<?php bloginfo('stylesheet_directory'); ?>/images/logo-emerald.png" alt="Emerald Street" width="284" height="88"></a></li>
-			<li><a href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-elle.png" alt="ELLE" width="277" height="96"></a></li>
-			<li><a href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-independent.png" alt="The Independent" width="299" height="69"></a></li>
-			<li><a href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-metro.png" alt="Metro" width="284" height="85"></a></li>
-			<li><a href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-sweatybetty.png" alt="Sweaty Betty" width="329" height="49"></a></li>
-			<li><a href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-mailonline.png" alt="Mail Online" width="272" height="44"></a></li>
-			<li><a href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-stylist.png" alt="Stylist" width="305" height="62"></a></li>
-			<li><a href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo-mailonline.png" alt="Mail Online" width="272" height="44"></a></li>
+		<?php if ( have_posts() ) :
+			while ( have_posts() ) : the_post();
+			$quotes_query = new WP_Query(array(
+				'sos_quotes' => 'press'
+			));
+			if($quotes_query->have_posts()) : while($quotes_query->have_posts()) : $quotes_query->the_post();
+				if ( has_post_thumbnail() ) {
+					global $post;
+					$thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
+					$image_url = $thumb['0'];
+				}
+				$quote_url = get_post_meta(get_the_ID(), '_url', true); ?>
+			<li><a href="<?php echo $quote_url; ?>"><img src="<?php echo $image_url; ?>" alt="<?php the_title(); ?>"></a></li>	
+			<?php endwhile; endif;
+		endwhile; endif; ?>
 		</ul>
 	</div>
 </aside>
-
-				<?php if ( have_posts() ) :
-					while ( have_posts() ) : the_post(); ?>
-					<h1><?php the_title(); ?></h1>
-					<?php
-					$quotes_query = new WP_Query(array(
-						'sos_quotes' => 'press'
-					)); ?>
-					<?php if($quotes_query->have_posts()) : while($quotes_query->have_posts()) : $quotes_query->the_post(); ?>
-					<?php the_content(); ?>
-					<hr>
-					<?php endwhile; endif; ?>
-				<?php endwhile; endif; ?>
 <?php
 get_footer();
 ?>
