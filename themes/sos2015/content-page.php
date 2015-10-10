1ce11b9bf87c4a1bba49c09f6d22f0e2d114b348
jayfresh <jonathan@penrosestudio.com>
2015-10-10 18:28:28 +0100

    healcode styling and content page width

--- themes/sos2015/content-page.php
+++ themes/sos2015/content-page.php
@@ -4,7 +4,9 @@
 	<div class="title-container">
 		<h1><?php the_title(); ?></h1>
 	</div>
-	<?php the_content(); ?>
 	<?php if ( has_post_thumbnail() ) { ?>
 		<?php the_post_thumbnail('content-page'); ?>
 	<?php } ?>
+	<div class="page-content-container">
+		<?php the_content(); ?>
+	</div>
