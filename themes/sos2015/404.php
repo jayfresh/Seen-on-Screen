2edfcf1c8fb9b6f8db418635a832dd40a71a7bfb
jayfresh <jonathan@penrosestudio.com>
2015-10-18 16:50:30 +0100

    style for 404 page and regular content pages

--- themes/sos2015/404.php
+++ themes/sos2015/404.php
@@ -2,19 +2,18 @@
 /* for the 404 page */
 get_header();
 ?>
-
-			<div class="contentPage">
-				<div class="contentColumn left">
-					<div class="title-container">
-						<h1>Oops!</h1>
-					</div>
-					<p>We can't find the page you are looking for. Perhaps you'd like to go back to the homepage and have another look? If you want to get in touch, send us an email at <a href="mailto:info@seenonscreenfitness.com">info@seenonscreenfitness.com</a></p>
-				</div>
-				<div class="eventbox left">
-					<img src="<?php bloginfo('stylesheet_directory'); ?>/images/roughs/rihanna-event.png">
-				</div>
-			</div>
-
+<?php /*
+	Sub-template for single pages
+*/ ?>
+	<div class="title-container">
+		<h1>Oops!</h1>
+	</div>
+	<?php if ( has_post_thumbnail() ) { ?>
+		<?php the_post_thumbnail('full'); ?>
+	<?php } ?>
+	<div class="page-content-container">
+		<p>Sorry, we can't find the page you are looking for. If you want to get in touch, send us an email at <a href="mailto:info@seenonscreenfitness.com">info@seenonscreenfitness.com</a></p>
+	</div>
 <?php
 get_footer();
 ?>
