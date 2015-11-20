d40feda0a5fb076a41f6e94ff60a7195ac83272f
Jonathan Lister <jonathan@pensionbee.com>
2015-11-20 15:14:48 +0000

    Create template-semi-transparent-content-page.php

---
+++ themes/sos2015/template-semi-transparent-content-page.php
@@ -0,0 +1,21 @@
+<?php
+/* Template: Semi-transparent Content Area */
+get_header();
+?>
+	<?php if ( have_posts() ) :
+		while ( have_posts() ) : the_post(); ?>
+			<div class="title-container">
+    		<h1><?php the_title(); ?></h1>
+    	</div>
+    	<?php if ( has_post_thumbnail() ) { ?>
+    		<?php the_post_thumbnail('full'); ?>
+    	<?php } ?>
+    	<div class="page-content-container page-content-container-semi-transparent">
+    		<?php the_content(); ?>
+    	</div>
+		<?php endwhile;
+	endif; ?>
+
+<?php
+get_footer();
+?>
