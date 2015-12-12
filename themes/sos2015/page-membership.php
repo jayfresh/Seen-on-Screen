fe30171fffb39f46fa25562e8d64e236463c807c
jayfresh <jonathan@penrosestudio.com>
2015-12-12 19:47:20 +0000

    changing membership page images and sign up links

--- themes/sos2015/page-membership.php
+++ themes/sos2015/page-membership.php
@@ -24,7 +24,7 @@ endif; ?>
 			</div>
 			<div class="quote-holder">
 				<blockquote>
-					<q>&ldquo;You don’t have to be a member  to do our classes or workshop&rdquo;</q>
+					<q>&ldquo;You don't have to be a member to do our classes or workshop&rdquo;</q>
 					<cite>Elle Magazine</cite>
 				</blockquote>
 			</div>
@@ -32,26 +32,26 @@ endif; ?>
 		<div class="signup-holder">
 			<div class="signup-block">
 				<div class="img-holder">
-					<img src="<?php bloginfo('stylesheet_directory'); ?>/images/card-all.png" alt="AAA #sosbackstage" width="209" height="683">
+					<img src="<?php bloginfo('stylesheet_directory'); ?>/images/membership-aaa.png" alt="Seen On Screen Unlimited Membership #sosbackstage" width="" height="">
 				</div>
 				<div class="text-holder">
-					<?php $membership =  get_page_by_title('Access All Areas'); ?>
+					<?php $membership = get_page_by_title('Access All Areas'); ?>
 					<?php echo wpautop($membership->post_content); ?>
 					<strong class="title">ACCESS ALL AREAS</strong>
 					<span class="rate">&pound;100 PER MONTH</span>
-					<a href="#" class="btn-signup">Sign Up Now</a>
+					<a href="https://clients.mindbodyonline.com/classic/ws?studioid=44775&stype=40" target="_blank" class="btn-signup">Sign Up Now</a>
 				</div>
 			</div>
 			<div class="signup-block">
 				<div class="img-holder">
-					<img src="<?php bloginfo('stylesheet_directory'); ?>/images/card-vip.png" alt="VIP #sosbackstage" width="207" height="684">
+					<img src="<?php bloginfo('stylesheet_directory'); ?>/images/membership-vip.png" alt="Seen On Screen Weekend Membership #sosbackstage" width="" height="">
 				</div>
 				<div class="text-holder">
-					<?php $membership =  get_page_by_title('V.I.P'); ?>
+					<?php $membership = get_page_by_title('V.I.P'); ?>
 					<?php echo wpautop($membership->post_content); ?>
 					<strong class="title">V.I.P</strong>
 					<span class="rate">&pound;75 PER MONTH</span>
-					<a href="#" class="btn-signup">Sign Up Now</a>
+					<a href="https://clients.mindbodyonline.com/classic/ws?studioid=44775&stype=40" target="_blank" class="btn-signup">Sign Up Now</a>
 				</div>
 			</div>
 		</div>
